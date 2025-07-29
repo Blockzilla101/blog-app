ed768ecdf3839a95c963171a74ace17d1e2725ff
Blockzilla101 <blockzilla101@gmail.com>
2025-07-29 19:06:29 +0500

    refactor blog list out of blog view

---
+++ src/components/BlogList.vue
@@ -0,0 +1,66 @@
+<script lang="ts" setup>
+
+import Blog from "./Blog.vue";
+import type { AccountInfo, BlogItem } from "../api/types.ts";
+
+defineProps({
+    account: {
+        type: Object as () => AccountInfo | undefined,
+        required: true,
+    },
+    blogs: {
+        type: Array as () => BlogItem[],
+        required: true,
+    },
+    allLoaded: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+defineEmits({
+    "load-more": () => Promise<void>,
+});
+
+function openBlog(blog: BlogItem) {
+    window.location.pathname = `/blog/${blog.uuid}`;
+}
+
+</script>
+
+<template>
+    <section class="blog-section">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <Blog v-for="blog in blogs" :key="blog.uuid" :account="account" :blog="blog" :preview="true"
+                  class="blog-preview" @click="openBlog(blog)"></Blog>
+        </div>
+        <button v-if="!allLoaded" class="btn" @click="$emit('load-more')">
+            Load More
+        </button>
+    </section>
+</template>
+
+<style scoped>
+
+.blog-section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.blog-preview {
+    cursor: pointer;
+    transition: 0.3s ease;
+}
+
+.blog-preview:hover {
+    background-color: var(--card-background-color-darker);
+}
+
+.btn {
+    margin-top: 2em;
+    background-color: var(--background-color);
+    backdrop-filter: blur(12px);
+}
+
+</style>
--- src/views/BlogsView.vue
+++ src/views/BlogsView.vue
@@ -3,7 +3,7 @@
 import type { BlogItem } from "../api/types.ts";
 import { ref } from "vue";
 import { Backend } from "../api/backend.ts";
-import Blog from "../components/Blog.vue";
+import BlogList from "../components/BlogList.vue";
 
 const blogs = ref<BlogItem[]>([
     await Backend.fetchBlog("1"),
@@ -24,45 +24,12 @@ async function loadMore() {
     }
 }
 
-function openBlog(blog: BlogItem) {
-    window.location.pathname = `/blog/${blog.uuid}`;
-}
-
 </script>
 
 <template>
-    <section class="blog-section">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <Blog v-for="blog in blogs" :key="blog.uuid" :account="account" :blog="blog" :preview="true"
-                  class="blog-preview" @click="openBlog(blog)"></Blog>
-        </div>
-        <button class="btn" @click="loadMore">
-            Load More
-        </button>
-    </section>
+    <BlogList :account="account" :all-loaded="blogs.length >= 20" :blogs="blogs" @load-more="loadMore"></BlogList>
 </template>
 
 <style scoped>
-
-.blog-section {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.blog-preview {
-    cursor: pointer;
-    transition: 0.3s ease;
-}
-
-.blog-preview:hover {
-    background-color: var(--card-background-color-darker);
-}
-
-.btn {
-    margin-top: 2em;
-    background-color: var(--background-color);
-    backdrop-filter: blur(12px);
-}
 
 </style>
